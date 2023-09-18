324b4b1b8d7ab8118f871eea5604e1466582ce31
buggy <nguyendanghoaa@gmail.com>
2023-09-18 16:55:41 +0700

    doing micro-service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+import { EventPattern, Payload } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @EventPattern('create_user')
+  handleUserCreate(@Payload() data: any) {
+    console.log('data', data);
   }
 }
